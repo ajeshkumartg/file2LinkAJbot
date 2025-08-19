ba3a65bcd85af9d9d6d33eb8b3ee575bfa557e61
AJESHKUMAR T G <46497585+ajeshkumartg@users.noreply.github.com>
2025-08-20 00:51:51 +0100

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ BOT_TOKEN = "" # Replace with your bot token
 
 # --- SERVER CONFIGURATION ---
 SERVER_IP = "" # Replace with your Oracle instance's Public IP
-DOWNLOAD_DIR = "/var/www/html/downloads/"
+DOWNLOAD_DIR = "" # Replace with the folder where you want the files saved
 
 # --- FILE CLEANUP CONFIGURATION ---
 FILE_LIFETIME_SECONDS = 6 * 3600  # 6 hours
